ed30d39ff315c98966cafedfd3ef8fd91a776f55
Marko <Marko.peric@me.com>
2017-10-23 21:32:32 +0200

    Zadatak 1.

--- IntegerList.cs
+++ IntegerList.cs
@@ -1,13 +1,17 @@
-﻿namespace raupjc_hw1
+﻿using System;
+
+namespace raupjc_hw1
 {
     public class IntegerList : IIntegerList
     {
 
         private int[] _internalStorage;
+        private int? _top;
         
         public IntegerList()
         {
             _internalStorage = new int[4];   
+            Console.WriteLine(_internalStorage[1]);
         }
 
         public IntegerList(int initialSize)
@@ -24,22 +28,61 @@
 
         public void Add(int item)
         {
-            throw new System.NotImplementedException();
+            if (_top != null && _internalStorage.Length <= _top + 1)
+            {
+                var oldSize = _internalStorage.Length;
+                int[] old = _internalStorage;
+                _internalStorage = new int[oldSize * 2];
+                for (int i = 0; i < oldSize; i++)
+                {
+                    _internalStorage[i] = old[i];
+                }
+            }
+
+            _top = (_top == null) ? 0 : _top+1;
+            _internalStorage[(int) _top] = item;
+            printArray();
         }
 
         public bool Remove(int item)
         {
-            throw new System.NotImplementedException();
+            for (int i = 0; i<= _top; i++) {
+                if (_internalStorage[i].Equals(item))
+                {
+                    return RemoveAt(i);
+                }
+            }
+
+            return false;
         }
 
         public bool RemoveAt(int index)
         {
-            throw new System.NotImplementedException();
+            if (index > _top)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            for (int i = index; i <= _top; i++)
+            {
+                _internalStorage[i] = _internalStorage[i+1];
+            }
+
+            _top--;
+            printArray();
+            return true;
         }
 
         public int GetElement(int index)
         {
-            throw new System.NotImplementedException();
+            if (index >= 0 && index <= _top)
+            {
+                return _internalStorage[index];
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
         }
 
         public int IndexOf(int item)
@@ -47,15 +90,34 @@
             throw new System.NotImplementedException();
         }
 
-        public int Count { get; }
+        public int Count => (_top != null) ? (int) _top + 1 : 0;
+
         public void Clear()
         {
-            throw new System.NotImplementedException();
+            _top = null;
         }
 
         public bool Contains(int item)
         {
-            throw new System.NotImplementedException();
+            for (int i = 0; i <= _top; i++)
+            {
+                if (_internalStorage[i].Equals(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void printArray()
+        {
+            Console.Write("[");
+            for (int i = 0; i <= _top; i++)
+            {
+                Console.Write(_internalStorage[i] + ", ");
+            }
+            Console.Write("]");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,6 +9,22 @@ namespace raupjc_hw1
         public static void Main(string[] args)
         {
             IntegerList lista = new IntegerList();
+            lista.Add(1);
+            lista.Add(2);
+            lista.Add(3);
+            lista.Add(4);
+            lista.Add(5);
+
+            lista.RemoveAt(0);
+            lista.Remove(5);
+            
+            Console.WriteLine(lista.Count);
+            Console.WriteLine(lista.Remove(100));
+            //Console.WriteLine(lista.RemoveAt(5));
+            
+            lista.Clear();
+            Console.WriteLine(lista.Count);
+
         }
     }
 }
