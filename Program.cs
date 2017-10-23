7e93b423909abd113c233eba6a4b2bdb6642d6dc
Marko <Marko.peric@me.com>
2017-10-23 12:28:12 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace raupjc_hw1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
